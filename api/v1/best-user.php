7a6714d758c7b3be23fd38a5f73e436e2443c25d
Hossein <ho3einzafari@gmail.com>
2020-07-26 14:49:58 +0430

    gender column update db and api

--- api/v1/best-user.php
+++ api/v1/best-user.php
@@ -1,7 +1,7 @@
 <?php
 require_once '../../core/loader.php';
 
-$result = $connection->query("select userId , name , family , image , point from t_user order by point DESC")->fetchAll();
+$result = $connection->query("select userId , name , family , image ,gender ,point from t_user order by point DESC")->fetchAll();
 
 setJsonOutPut();
 echo json_encode($result);
