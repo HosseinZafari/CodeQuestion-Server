a46b92879aeb450f661eb2def934b5191c13641d
Hossein <ho3einzafari@gmail.com>
2020-08-01 15:46:46 +0430

    Add Login Api

---
+++ api/v1/login.php
@@ -0,0 +1,50 @@
+<?php 
+require_once '../../core/loader.php';
+
+if(!isset($_POST['email']) || !isset($_POST['password'])){
+     output(['status' => 'Error' , 'code' => 400 , 'msg' => 'لطفا تمامی فیلد ها را پر کنید']);
+}
+ 
+$email       = $_POST['email'];
+$password    = $_POST['password'];
+
+$user = getUserByEmail($connection , $email);
+if(sizeOf($user) == 0){
+    output(['status' => 'failed' , 'code' => 400 , 'msg' => 'حسابی با این ایمیل یافت نشد']);
+}
+
+if(password_verify($password , $user[0]['password'])){
+    $shell = $connection->prepare('SELECT userId , name , family , phone , image , phone , email , gender , role ,point FROM t_user WHERE email=:email');
+    $shell->bindParam(':email' , $email);
+
+    try {
+        $shell->execute();
+        $result = $shell->fetchAll(PDO::FETCH_ASSOC);
+        if(!empty($result)){
+            output(['status' => 'success' , 'code' => 200 , 'msg' => 'خوش آمدید' , 'user' => $result]);
+        } else {
+            output(['status' => 'Error' , 'code' => 400, 'msg' => 'مشکلی در ورود به حساب شما رخ داده است!']);
+        }
+    } catch(Exception $e) {
+        ouput(['status' => 'Error' , 'code' => 400, 'msg' => 'مشکلی در ورود به حساب شما رخ داده است!']);
+    }
+} else {
+    output(['status' => 'Error' , 'code' => 400, 'msg' => 'رمز عبور شما صحیح نیست']);
+}
+
+
+
+function getCountUserByEmail($connection , $email){
+ return sizeOf(getUserByEmail($connection , $email));   
+}
+
+function getFirstUser($connection , $email){
+    return getUserByEmail($connection , $email)[0];
+}
+
+function getUserByEmail($connection , $email): array{
+    $shell = $connection->prepare("SELECT * FROM t_user WHERE email=:email");
+    $shell->bindParam(':email' , $email);
+    $shell->execute();
+    return $shell->fetchAll(PDO::FETCH_ASSOC);
+}
