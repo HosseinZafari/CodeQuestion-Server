bed9db900669da0ef218ca2207b0fc9bd114cb3a
Hossein Zafari <ho3einzafari@gmail.com>
2020-08-12 15:38:25 +0430

    Add Rules Api

--- api/v1/rules.php
+++ api/v1/rules.php
@@ -4,10 +4,10 @@ require_once '../../core/loader.php';
 
 try {
     global $connection;
-    $command = $connection->prepare('SELECT * FROM t_role');
+    $command = $connection->prepare('SELECT * FROM t_rule');
     $command->execute();
     $rows = $command->fetchAll();
-    output(['code' => 200 ,'status' => 'success' ,  'msg' => 'لیست قوانین' , 'roles' => $rows]);
+    output(['code' => 200 ,'status' => 'success' ,  'msg' => 'لیست قوانین' , 'rules' => $rows]);
 } catch(Exception $e){
     output(['code' => 500 , 'status' => 'failed' , 'msg' => 'خطایی در گرفتن اطلاعات به وجود آمده لطفا بعدا امتحان کنید']);
 }
